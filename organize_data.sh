0783f90b439c3f8417f467723d9796574b5a6464
porinky0424 <daichi5967@docomo.ne.jp>
2022-12-08 21:36:42 +0900

    feat: Add script for running only organize_data

---
+++ organize_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export PATH=/home/app/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/home/app/cuda/lib64:$LD_LIBRARY_PATH
+
+# `bash prganize_data.sh $natt $nval $cvoc $clen 実験id 実験id ...`で実行（実験idは何個でも指定できる）
+# organize_data.pyのみを走らせる
+
+natt=$1; shift
+nval=$1; shift
+cvoc=$1; shift
+clen=$1; shift
+
+for id in "$@"; do
+    sbatch -p p --gres=gpu:1 \
+    --wrap "python codes/organize_data.py result/$id--$natt-$nval-$cvoc-$clen.txt"
+done
